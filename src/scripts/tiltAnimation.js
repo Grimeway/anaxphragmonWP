17dfed44550d79277ac9c7608036649e62710bba
Grimeway <45391099+Grimeway@users.noreply.github.com>
2026-09-06 12:08:27 +0200

    Optimize homepage images, accessibility, and layout

--- src/scripts/tiltAnimation.js
+++ src/scripts/tiltAnimation.js
@@ -1,6 +1,12 @@
 import VanillaTilt from "vanilla-tilt";
 
 export default function initTiltAnimation() {
+  if (
+    window.matchMedia("(prefers-reduced-motion: reduce)").matches ||
+    !window.matchMedia("(hover: hover) and (pointer: fine)").matches
+  )
+    return;
+
   const elements = document.querySelectorAll(".js-tilt");
   VanillaTilt.init(elements);
 }
